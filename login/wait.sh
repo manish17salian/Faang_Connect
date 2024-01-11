0df2a44a636ddbab3f9f819d77457ac89bc211d3
manish17salian <manishsalian16@gmail.com>
2024-01-11 01:33:27 +0000

    Added Wait.sh for docker initialisation order

--- login/wait.sh
+++ login/wait.sh
@@ -8,9 +8,6 @@ port="$2"
 cmd="$3"
 
 until (echo > /dev/tcp/$host/$port) >/dev/null 2>&1; do
-  echo "Host: $host"
-  echo "Port: $port"
-  echo "cmd: $cmd"
   echo "Waiting for RabbitMQ ($host:$port) to start..."
   sleep 1
 done
--- netflix/wait.sh
+++ netflix/wait.sh
@@ -5,15 +5,23 @@ set -e
 
 host="$1"
 port="$2"
-cmd="$3"
+eureka_host="eureka-server"
+eureka_port="$3"
+cmd="$4"
 
 until (echo > /dev/tcp/$host/$port) >/dev/null 2>&1; do
-  echo "Host: $host"
-  echo "Port: $port"
-  echo "cmd: $cmd"
   echo "Waiting for RabbitMQ ($host:$port) to start..."
   sleep 1
 done
 
 echo "RabbitMQ is up - executing command"
+
+
+# Wait for Eureka
+until (echo > /dev/tcp/$eureka_host/$eureka_port) >/dev/null 2>&1; do
+  echo "Waiting for Eureka ($eureka_host:$eureka_port) to start..."
+  sleep 1
+done
+
+echo "Eureka is up - executing command"
 exec $cmd
